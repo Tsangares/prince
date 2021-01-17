0c02624db813635fc8f068c7d8273425ead465ac
William Wyatt <tsangares@gmail.com>
2021-01-17 15:36:39 -0800

    Changed from tabs to spaces

--- distance.py
+++ distance.py
@@ -19,9 +19,9 @@ count = getCount()
 distance = 100
 
 def close(signal, frame):
-	print("\nTurning off ultrasonic distance detection...\n")
-	GPIO.cleanup() 
-	sys.exit(0)
+        print("\nTurning off ultrasonic distance detection...\n")
+        GPIO.cleanup() 
+        sys.exit(0)
 
 signal.signal(signal.SIGINT, close)
 
@@ -32,38 +32,38 @@ GPIO.setup(pinEcho, GPIO.IN)
 counted = False
 
 while True:
-	# set Trigger to HIGH
-	GPIO.output(pinTrigger, True)
-	# set Trigger after 0.01ms to LOW
-	time.sleep(0.00001)
-	GPIO.output(pinTrigger, False)
+        # set Trigger to HIGH
+        GPIO.output(pinTrigger, True)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(pinTrigger, False)
 
-	startTime = time.time()
-	stopTime = time.time()
+        startTime = time.time()
+        stopTime = time.time()
 
-	# save start time
-	while 0 == GPIO.input(pinEcho):
-		startTime = time.time()
+        # save start time
+        while 0 == GPIO.input(pinEcho):
+                startTime = time.time()
 
-	# save time of arrival
-	while 1 == GPIO.input(pinEcho):
-		stopTime = time.time()
+        # save time of arrival
+        while 1 == GPIO.input(pinEcho):
+                stopTime = time.time()
 
-	# time difference between start and arrival
-	TimeElapsed = stopTime - startTime
-	# multiply with the sonic speed (34300 cm/s)
-	# and divide by 2, because there and back
-	distance = (TimeElapsed * 34300) / 2
+        # time difference between start and arrival
+        TimeElapsed = stopTime - startTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
 
-	print (f"Distance: {distance:.01f} cm")
+        print (f"Distance: {distance:.01f} cm")
 
 
-	if(distance<=100 and not counted):
-		count += 1
+        if(distance<=100 and not counted):
+                count += 1
                 counted = True
                 setCount(count)
-		print(f'Current count is {count}')
+                print(f'Current count is {count}')
         else:
                 counted = False
-	time.sleep(1)
+        time.sleep(1)
 
